e338476d845fbe2061d0cbb414d46e9eceafec61
ed-devops-d2 <devops-d2@eurodyn.com>
2023-06-30 14:45:11 +0300

    Fix: release 3.0.0

--- docs/assets/js/ab74497b.f2724be9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkesthesis_core=self.webpackChunkesthesis_core||[]).push([[6084],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
